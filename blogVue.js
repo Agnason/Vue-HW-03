ae7cf47480423ff32208f06f068a44f78d50cfed
Насонов Александр Геннадьевич <anasonov@nevalab.ru>
2023-06-27 20:05:18 +0300

    start

---
+++ blogVue.js
@@ -0,0 +1,70 @@
+
+
+new Vue({
+
+    el: '#app',
+    data: {
+        items: [
+            {
+                id: 1,
+                imgSrc: 'img/Blog_Image01.png',
+                imgAlt: 'Blog_Image01',
+                itemTitle: 'Let’s Get Solution For Building Construction Work',
+                itemText: '26 December,2022',
+                imgSrcIcon: 'img/icon.svg',
+                imgSrcIconAlt: 'icon',
+                btnText: 'Kitchan Design'
+            },
+            {
+                id: 2,
+                imgSrc: 'img/Blog_Image02.png',
+                imgAlt: 'Blog_Image02',
+                itemTitle: 'Low Cost Latest Invented Interior Designing',
+                itemText: '22 December,2022',
+                imgSrcIcon: 'img/icon.svg',
+                imgSrcIconAlt: 'icon',
+                btnText: 'Living Design'
+            },
+            {
+                id: 3,
+                imgSrc: 'img/Blog_Image03.png',
+                imgAlt: 'Blog_Image03',
+                itemTitle: 'Best For Any Office & Business Interior',
+                itemText: '25 December,2022',
+                imgSrcIcon: 'img/icon.svg',
+                imgSrcIconAlt: 'icon',
+                btnText: 'Interior Design'
+            },
+            {
+                id: 4,
+                imgSrc: 'img/Blog_Image01.png',
+                imgAlt: 'Blog_Image01',
+                itemTitle: 'Let’s Get Solution For Building Construction Work',
+                itemText: '26 December,2022',
+                imgSrcIcon: 'img/icon.svg',
+                imgSrcIconAlt: 'icon',
+                btnText: 'Kitchan Design'
+            },
+            {
+                id: 5,
+                imgSrc: 'img/Blog_Image02.png',
+                imgAlt: 'Blog_Image02',
+                itemTitle: 'Low Cost Latest Invented Interior Designing',
+                itemText: '22 December,2022',
+                imgSrcIcon: 'img/icon.svg',
+                imgSrcIconAlt: 'icon',
+                btnText: 'Living Design'
+            },
+            {
+                id: 6,
+                imgSrc: 'img/Blog_Image03.png',
+                imgAlt: 'Blog_Image03',
+                itemTitle: 'Best For Any Office & Business Interior',
+                itemText: '25 December,2022',
+                imgSrcIcon: 'img/icon.svg',
+                imgSrcIconAlt: 'icon',
+                btnText: 'Interior Design'
+            }
+        ]
+    }
+})
